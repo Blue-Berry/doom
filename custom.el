61b184d785ab413de478a44cca10c164660cb397
Liam Berry <liamandberry@gmail.com>
2025-07-15 18:46:05 +0200

    appearance

--- custom.el
+++ custom.el
@@ -4,6 +4,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
+     "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d"
+     "7771c8496c10162220af0ca7b7e61459cb42d18c35ce272a63461c0fc1336015" default))
  '(ignored-local-variable-values '((lsp-enabled-clients nixd))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
