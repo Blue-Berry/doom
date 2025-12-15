ccb65bcb131123ff87dca17cbed9ae6e9927c9a3
Liam Berry <liamandberry@gmail.com>
2025-12-15 10:43:30 +0200

    adjust line numbers

--- custom.el
+++ custom.el
@@ -5,7 +5,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098"
+   '("83550d0386203f010fa42ad1af064a766cfec06fc2f42eb4f2d89ab646f3ac01"
+     "9b9d7a851a8e26f294e778e02c8df25c8a3b15170e6f9fd6965ac5f2544ef2a9"
+     "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098"
      "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
      "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d"
      "7771c8496c10162220af0ca7b7e61459cb42d18c35ce272a63461c0fc1336015" default))
@@ -17,3 +19,4 @@
  ;; If there is more than one, they won't work right.
  )
 (put 'projectile-grep 'disabled nil)
+(put 'projectile-ripgrep 'disabled nil)
